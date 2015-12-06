ace90e4c86edb917ae08b42fe7ca55403871c73f
KateeR95 <katee.riverside@gmail.com>
2015-12-06 23:13:53 +0400

    Outland

---
+++ Source/Galaxy.Environments/Actors/BulletER.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using System.Drawing;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Galaxy.Environments.Actors
+{
+  
+    public class BulletER : BaseActor
+     {
+            #region Constant
+            private static int Speed = 10;
+            #endregion
+  
+ 
+            #region Constructors
+
+            public BulletER(ILevelInfo info) : base(info)
+             {
+                 Width = 6;
+                 Height = 12;
+                ActorType = ActorType.Enemy;
+             }
+
+           #endregion
+
+           private int si;
+
+           public int Outland
+        {
+            get { return si; }
+            set { si = value; }
+        }
+           
+            public override bool IsAlive
+            {
+                get
+                {
+                    return base.IsAlive;
+                }
+                set
+                {
+                    base.IsAlive = value;
+                    CanDrop = value;
+                }
+            }
+            private void bulletERRemove()
+            {
+               if (Position.Y > 480 || Position.Y < 0)
+               {
+                   IsAlive = false;
+                   CanDrop = true;
+               }
+                
+            }
+ 
+ 
+  #region Overrides
+
+ 
+  public override void Load()
+   {
+      Load(@"Assets\bullet.png");
+   }
+
+
+  public override void Update()
+      {
+          bulletERRemove();
+          Position = new Point(Position.X, Position.Y + Speed);
+      }
+
+    #endregion
+  
+  }
+} 
+
+
---
+++ Source/Galaxy.Environments/Actors/Superman.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+namespace Galaxy.Environments.Actors
+{
+    class Superman : BaseActor
+    {
+        #region Constant
+
+        private const int MaxSpeed = 1;
+        private const long StartFlyMs = 2000;
+
+        #endregion
+
+        #region Private fields
+
+        protected bool m_flying;
+        protected Stopwatch m_flyTimer;
+
+        #endregion
+
+        #region Constructors
+
+        public Superman(ILevelInfo info)
+            : base(info)
+        {
+            Width = 30;
+            Height = 30;
+            ActorType = ActorType.Superman;
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (!IsAlive)
+                return;
+
+            if (!m_flying)
+            {
+                if (m_flyTimer.ElapsedMilliseconds <= StartFlyMs) return;
+
+                m_flyTimer.Stop();
+                m_flyTimer = null;
+                h_changePosition();
+                m_flying = true;
+            }
+            else
+            {
+                h_changePosition();
+            }
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override void Load()
+        {
+            Load(@"Assets\superm.png");
+            if (m_flyTimer == null)
+            {
+                m_flyTimer = new Stopwatch();
+                m_flyTimer.Start();
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+        public override bool IsAlive
+        {
+            get
+            {
+                return base.IsAlive;
+            }
+            set
+            {
+                base.IsAlive = true;
+                CanDrop = false;
+            }
+        }
+
+        private void h_changePosition()
+        {
+            int speed = 10;
+            Position = new Point(Position.X + speed, Position.Y + speed );
+        }
+
+        #endregion
+    }
+}
